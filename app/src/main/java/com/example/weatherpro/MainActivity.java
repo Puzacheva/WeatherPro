790ee4311a0013f000b9229725ff7f5e5242eca7
olgaIsFF <olga.isfff@gmail.com>
2024-11-05 01:55:15 +0300

    -

---
+++ app/src/main/java/com/example/weatherpro/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.weatherpro;
+
+import android.os.Bundle;
+import androidx.activity.ComponentActivity;
+import androidx.compose.runtime.Composable;
+import androidx.compose.ui.Modifier;
+import androidx.compose.ui.tooling.preview.Preview;
+
+
+class MainActivity {
+}
